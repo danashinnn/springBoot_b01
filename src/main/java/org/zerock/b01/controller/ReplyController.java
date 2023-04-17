1e3ba0f915a452aa2b87c093dfa0b3870f47e66d
danashinnn <103996283+danashinnn@users.noreply.github.com>
2023-04-17 21:49:02 +0900

    잘못되는 상황 처리(500 에러를 400 에러로)

--- src/main/java/org/zerock/b01/controller/ReplyController.java
+++ src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -1,6 +1,7 @@
 package org.zerock.b01.controller;
 
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.b01.dto.ReplyDTO;
+import org.zerock.b01.service.ReplyService;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -19,7 +21,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("/replies")
 @Log4j2
+@RequiredArgsConstructor // 의존성 주입을 위한
 public class ReplyController {
+
+    private final ReplyService replyService;
 
     @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글 등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -34,7 +39,10 @@ public class ReplyController {
         }
 
         Map<String, Long> resultMap = new HashMap<>();
-        resultMap.put("rno", 111L);
+
+        Long rno = replyService.register(replyDTO);
+
+        resultMap.put("rno", rno);
 
         return resultMap;
     }
