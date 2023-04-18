1564a97ad2ebdfff32ecefdf2ab40c73ddd2bb69
danashinnn <103996283+danashinnn@users.noreply.github.com>
2023-04-18 23:15:05 +0900

    특정 게시물의 목록, 조회, 수정, 삭제

--- src/main/java/org/zerock/b01/controller/ReplyController.java
+++ src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -7,10 +7,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.dto.ReplyDTO;
 import org.zerock.b01.service.ReplyService;
 
@@ -44,6 +43,40 @@ public class ReplyController {
 
         resultMap.put("rno", rno);
 
+        return resultMap;
+    }
+
+    @ApiOperation(value = "Replies of Board", notes = "GET 방식으로 특정 게시물의 댓글 목록")
+    @GetMapping(value = "/list/{bno}")
+    public PageResponseDTO<ReplyDTO> getList(@PathVariable("bno") Long bno, // 호출하는 경로의 값을 직접 파라미터의 변수로 처리
+                                             PageRequestDTO pageRequestDTO) {
+        PageResponseDTO<ReplyDTO> responseDTO = replyService.getListOfBoard(bno, pageRequestDTO);
+        return responseDTO;
+    }
+
+    @ApiOperation(value = "Read Reply", notes = "GET 방식으로 특정 댓글 조회")
+    @GetMapping("/{rno}")
+    public ReplyDTO getReplyDTO(@PathVariable("rno") Long rno) {
+        ReplyDTO replyDTO = replyService.read(rno);
+        return replyDTO;
+    }
+
+    @ApiOperation(value = "Delete Reply", notes = "DELETE 방식으로 특정 댓글 삭제")
+    @DeleteMapping("/{rno}")
+    public Map<String, Long> remove(@PathVariable("rno") Long rno) {
+        replyService.remove(rno);
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno", rno);
+        return resultMap;
+    }
+
+    @ApiOperation(value = "Modify Reply", notes = "PUT 방식으로 특정 댓글 수정")
+    @PutMapping(value = "/{rno}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Long> remove(@PathVariable("rno") Long rno, @RequestBody ReplyDTO replyDTO) {
+        replyDTO.setRno(rno);   // 번호를 일치시킴
+        replyService.modify(replyDTO);
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno", rno);
         return resultMap;
     }
 }
