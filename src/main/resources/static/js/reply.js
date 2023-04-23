249a08ec8b6499f664db6f40a5ae88f1e7541b02
danashinnn <103996283+danashinnn@users.noreply.github.com>
2023-04-23 22:52:17 +0900

    댓글 목록을 화면에 출력

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -1,4 +1,9 @@
-async function get1(bno) {
-    const result = await axios.get(`/replies/list/${bno}`)
-    return result;
+// async function get1(bno) {
+//     const result = await axios.get(`/replies/list/${bno}`)
+//     return result;
+// }
+
+async function getList({bno, page, size, goLast}) {
+    const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
+    return result.data
 }
