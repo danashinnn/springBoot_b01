e53e3ec42534f1c3e4c66608d9c668f47a358d0d
danashinnn <103996283+danashinnn@users.noreply.github.com>
2023-05-23 21:51:06 +0900

    Board와 BoardImage와 댓글 개수를 한번에 처리하는 BoardListAllDTO 구성

--- src/main/java/org/zerock/b01/repository/search/BoardSearch.java
+++ src/main/java/org/zerock/b01/repository/search/BoardSearch.java
@@ -3,6 +3,7 @@ package org.zerock.b01.repository.search;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.zerock.b01.domain.Board;
+import org.zerock.b01.dto.BoardListAllDTO;
 import org.zerock.b01.dto.BoardListReplyCountDTO;
 
 public interface BoardSearch {
@@ -13,5 +14,5 @@ public interface BoardSearch {
     // 목록 처리는 Querydsl을 이용하는 구조이므로 추가 메소드 구현 
     Page<BoardListReplyCountDTO> searchWithReplyCount(String[] types, String keyword, Pageable pageable);
 
-    Page<BoardListReplyCountDTO> searchWithAll(String[] types, String keyword, Pageable pageable);
+    Page<BoardListAllDTO> searchWithAll(String[] types, String keyword, Pageable pageable);
 }
